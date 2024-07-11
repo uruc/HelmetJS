440bb3e7f8fc084219af9925faa650e3ed6f6a9c
Glitch (perfect-working-cheetah) <none>
2024-07-11 23:22:22 +0000

    📫📺 Checkpoint ./myApp.js:69073492/6092

--- myApp.js
+++ myApp.js
@@ -11,8 +11,13 @@ app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 app.use(helmet.dnsPrefetchControl());
-
-
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", 'trusted-cdn.com']
+  }
+}));
 
 
 
