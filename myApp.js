7d0eca0506aa5a3bdc66b9f4675a0558c5bdb733
Glitch (perfect-working-cheetah) <none>
2024-07-11 23:45:37 +0000

    🔑⚽️ Checkpoint ./myApp.js:69073492/1257

--- myApp.js
+++ myApp.js
@@ -4,18 +4,20 @@ const helmet = require('helmet');
 
 const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({ action: 'deny' }));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", 'trusted-cdn.com']
+app.use(helmet({
+  hidePoweredBy: {},
+  frameguard: { action: 'deny' },
+  xssFilter: {},
+  noSniff: {},
+  ieNoOpen: {},
+  hsts: { maxAge: ninetyDaysInSeconds, force: true },
+  dnsPrefetchControl: {},
+  noCache: {},
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", 'trusted-cdn.com']
+    }
   }
 }));
 
