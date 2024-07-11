03156bff2f4b99ef94c3589959a8d448499512d6
Glitch (perfect-working-cheetah) <none>
2024-07-11 23:11:15 +0000

    📫🌺 Checkpoint ./myApp.js:69073492/204

--- myApp.js
+++ myApp.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'deny' }));
 app.use(helmet.xssFilter());
-
-
-
-
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
+app.use(helmet.dnsPrefetchControl());
 
 
 
